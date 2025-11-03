ba1e7ebf294a671faf59fed152c4f0085c78648f
unknown <emmco96@gmail.com>
2025-11-03 19:01:23 +0100

    new

--- firebase.js
+++ firebase.js
@@ -1,14 +1,20 @@
-require('dotenv').config();
-const admin = require('firebase-admin');
+require("dotenv").config();
+const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: serviceAccount.project_id,
-      clientEmail: serviceAccount.client_email,
-      privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
+      type: process.env.FIREBASE_TYPE,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      clientId: process.env.FIREBASE_CLIENT_ID,
+      authUri: process.env.FIREBASE_AUTH_URI,
+      tokenUri: process.env.FIREBASE_TOKEN_URI,
+      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+      universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
     }),
     databaseURL: "https://connecta-f12c3-default-rtdb.firebaseio.com",
   });
